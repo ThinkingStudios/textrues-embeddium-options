4c722d23fd30cffabf87343afc3b51a229ea2e6c
TexTrue <3140846162@qq.com>
2023-09-11 08:28:08 +0800

    fix & TRO stub

---
+++ src/main/java/me/flashyreese/mods/reeses_sodium_options/TROStub.java
@@ -0,0 +1,13 @@
+package me.flashyreese.mods.reeses_sodium_options;
+
+import net.minecraftforge.fml.IExtensionPoint;
+import net.minecraftforge.fml.ModLoadingContext;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.network.NetworkConstants;
+
+@Mod("reeses_sodium_options")
+public class TROStub {
+    public TROStub() {
+        ModLoadingContext.get().registerExtensionPoint(IExtensionPoint.DisplayTest.class, () -> new IExtensionPoint.DisplayTest(() -> NetworkConstants.IGNORESERVERONLY, (a, b) -> true));
+    }
+}
